5786ddf587ea44c79db4bb431be383c14ae42408
Lord_Detson <babanin.dima@gmail.com>
2019-10-09 23:23:19 +0300

    feat: implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.isLower = false;
+        this.getMiddle();
     }
 
     guess() {
-
+        return this.middle;
     }
 
     lower() {
-
+        this.isLower = true;
+        this.max = this.middle;
+        this.getMiddle();
     }
 
     greater() {
+        this.isLower = false;
+        this.min = this.middle;
+        this.getMiddle();
+    }
 
+    getMiddle() {
+        let result = Math.ceil((this.max + this.min) / 2);
+        this.middle = result;
     }
 }
 
